3143aaacabb1540d015978c7fb61925c2f225bd1
nuguseyo024 <nuguseyo024@naver.com>
2022-03-15 22:24:31 +0900

    6강 : 인터페이스 만들고 최초 상태값 false 세팅에 넣어줌

--- bubble/src/bubble/test/ex03/Player.java
+++ bubble/src/bubble/test/ex03/Player.java
@@ -3,43 +3,69 @@ package bubble.test.ex03;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Player extends JLabel{
-	
+// class player :: new 가능한 애들! 게임에 존재할 수 있음. (추상 메서드를 가질 수 없다) 
+public class Player extends JLabel implements Moveable {
+
+	// 위치 상태 
 	private int x;
 	private int y;
+
+	// 움직임 상태 
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
 	
 	private ImageIcon playerR, playerL;
-	
-	
+
 	public Player() {
 		initObject();
 		initSetting();
 	}
-	
+
 	private void initObject() {
 		playerR = new ImageIcon("image/playerR.png");
 		playerL = new ImageIcon("image/playerL.png");
-		
+
 	}
-	
+
 	private void initSetting() {
 		x = 55;
-		y = 535;	
+		y = 535;
+
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+		
 		
 		setIcon(playerR);
-		setSize(50,50);
-		setLocation(x,y);
+		setSize(50, 50);
+		setLocation(x, y);
 	}
 
-	
+	@Override
+	public void left() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void right() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
