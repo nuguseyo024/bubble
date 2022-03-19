725a9216ea576575d9855b603ccec87da7340ffa
nuguseyo024 <nuguseyo024@naver.com>
2022-03-19 19:19:16 +0900

    16강 - 어댑터 패턴과 인터페이스 default

---
+++ bubble/src/bubble/test/ex11/Bubble.java
@@ -0,0 +1,61 @@
+package bubble.test.ex11;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bubble extends JLabel {
+
+	// 의존성 컴포지션 
+	private Player player; // 플레이어의 정보는 생성자로 받아와야 돼요 컴포지션을 할때는 
+
+	// 위치 상태
+	private int x;
+	private int y;
+
+	// 움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+
+	// 적군을 맞춘 상태
+	private int state; // 0 : 물방울, 1 : 적을 가둔 물방울
+
+	private ImageIcon bubble; // 기본 물방울
+	private ImageIcon bubbled; // 적을 가둔 물방울
+	private ImageIcon bomb; // 물방울이 터진 상태
+
+	public Bubble(Player player) {
+		this.player = player;
+		
+		initObject();
+		initSetting();
+
+	}
+
+	private void initObject() {
+		bubble = new ImageIcon("image/bubble.png");
+		bubbled = new ImageIcon("image/bubbled.png");
+		bomb = new ImageIcon("image/bomb.png");
+
+	}
+
+	private void initSetting() {
+		left = false;
+		right = false;
+		up = false;
+		
+		x = player.getX();
+		y = player.getY();
+		
+		setIcon(bubble);
+		setSize(50,50);
+		
+		state = 0;
+	}
+
+}
