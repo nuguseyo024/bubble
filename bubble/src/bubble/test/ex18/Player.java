f64a6412dde8debe87300b0dd2cd46fc93f45fbe
nuguseyo024 <nuguseyo024@naver.com>
2022-03-22 16:01:30 +0900

    자바 버블버블 26강 - 적군 자동으로 움직이기

---
+++ bubble/src/bubble/test/ex18/Player.java
@@ -0,0 +1,181 @@
+package bubble.test.ex18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// class player :: new 가능한 애들! 게임에 존재할 수 있음. (추상 메서드를 가질 수 없다) 
+@Getter
+@Setter 
+public class Player extends JLabel implements Moveable {
+
+	private BubbleFrame mContext;
+	private List<Bubble> bubbleList;
+	
+	// 위치 상태 
+	private int x;
+	private int y;
+
+	// 플레이어의 방향 
+	private PlayerWay playerWay;
+	
+	// 움직임 상태 
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	// 벽에 충돌한 상태
+	private boolean leftWallCrash;
+	private boolean rightWallCrash;
+	
+	// 플레이어 속도 상태 
+	// 스피드는 고정값이라 상수로 입력, 상수는 대문자로. 
+	private final int SPEED = 4;
+	private final int JUMPSPEED = 2; // up, down 의 스피드 
+	
+	private ImageIcon playerR, playerL;
+
+	public Player(BubbleFrame mContext) {
+		this.mContext = mContext;
+		initObject();
+		initSetting();
+		initBackgroundPlayerService();
+	}
+
+
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+		
+		bubbleList = new ArrayList<>();
+	}
+
+	private void initSetting() {
+		x = 70;
+		y = 535;
+
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+		
+		leftWallCrash = false;
+		rightWallCrash = false;
+		
+		playerWay = playerWay.RIGHT;
+		
+		setIcon(playerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+
+	private void initBackgroundPlayerService() {
+		// BackgroundPlayerService 클래스 자체가 runnable 타입 (Runnable 을 상속받았기 때문에  
+		new Thread(new BackgroundPlayerService(this)).start();
+	
+	}
+	@Override
+	public void attack() {
+		new Thread(()->{
+			Bubble bubble = new Bubble(mContext);
+			mContext.add(bubble);
+			bubbleList.add(bubble);
+			if(playerWay == PlayerWay.LEFT) {
+				bubble.left();
+			}else {
+				bubble.right();
+			}
+		}).start();
+	}
+	
+	
+	@Override
+	public void left() {
+		playerWay = PlayerWay.LEFT;
+		
+		left = true;
+		new Thread(()-> {
+			while(left) {
+				setIcon(playerL);
+				x = x - SPEED;
+				setLocation(x,y);
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}		
+		}).start();
+
+	}
+         
+	@Override
+	public void right() {
+		playerWay = PlayerWay.RIGHT;
+		
+		right = true;
+		new Thread(()-> {
+			while(right) {
+				setIcon(playerR);
+				x = x + SPEED;
+				setLocation(x,y);
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}		
+		}).start();
+
+	}
+	
+	// left + up, rigth + up
+	@Override
+	public void up() {
+		//System.out.println("up");
+		up = true;
+		new Thread(()->{
+			for(int i = 0 ; i < 130/JUMPSPEED; i++) {
+				y = y - JUMPSPEED;
+				setLocation(x,y);
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			up = false;
+			down();
+			
+		}).start();
+	}
+
+	@Override
+	public void down() {
+		
+		System.out.println("down");
+		down = true;
+		new Thread(()->{
+			while(down) {
+				y = y + JUMPSPEED;
+				setLocation(x,y);
+				try {
+					Thread.sleep(3);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			down = false;
+		}).start();
+		
+	}
+
+}
